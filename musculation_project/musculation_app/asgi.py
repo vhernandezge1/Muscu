3090ffe8020092d6b916656aea9a7053487d8bac
valen <valentinhernandez420@yahoo.fr>
2024-12-09 14:18:05 +0100

    up

--- musculation_project/musculation_app/asgi.py
+++ musculation_project/musculation_app/asgi.py
@@ -2,15 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from musculation_project.routing import websocket_urlpatterns
+from coaching.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musculation_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musculation_project.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
+)
